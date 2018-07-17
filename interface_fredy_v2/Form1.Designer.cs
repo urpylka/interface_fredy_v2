93e3d805c157702abb221aa77816eafefb4c2c8b
Artem Smirnov <urpylka@gmail.com>
2018-07-17 16:00:13 +0400

    first commit

---
+++ interface_fredy_v2/Form1.Designer.cs
@@ -0,0 +1,229 @@
+﻿namespace interface_fredy_v2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
+            this.maskedTextBox2 = new System.Windows.Forms.MaskedTextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(214, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(121, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Обновить список";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(214, 68);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(121, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Подключиться";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(214, 41);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 248);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(166, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Статус соединения: Отключено";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(12, 12);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(181, 23);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Активировать приводы";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.KeyDown += new System.Windows.Forms.KeyEventHandler(this.button3_KeyDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 261);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(186, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Статус приводов: Деактивированы";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(214, 97);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(121, 148);
+            this.textBox1.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 38);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(127, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Выполнено операций: 0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(9, 136);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(160, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Задержка для двигателей, мс";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(152, 13);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Скорость двигателей, 0..255";
+            // 
+            // maskedTextBox1
+            // 
+            this.maskedTextBox1.Location = new System.Drawing.Point(12, 113);
+            this.maskedTextBox1.Name = "maskedTextBox1";
+            this.maskedTextBox1.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox1.TabIndex = 12;
+            this.maskedTextBox1.Text = "230";
+            // 
+            // maskedTextBox2
+            // 
+            this.maskedTextBox2.Location = new System.Drawing.Point(12, 152);
+            this.maskedTextBox2.Name = "maskedTextBox2";
+            this.maskedTextBox2.Size = new System.Drawing.Size(100, 20);
+            this.maskedTextBox2.TabIndex = 13;
+            this.maskedTextBox2.Text = "50";
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(12, 178);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(181, 23);
+            this.button4.TabIndex = 14;
+            this.button4.Text = "Записать задержку и скорость";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(214, 251);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(121, 23);
+            this.button5.TabIndex = 15;
+            this.button5.Text = "Обновить";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(347, 283);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.maskedTextBox2);
+            this.Controls.Add(this.maskedTextBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Терминал управления fredy_v2";
+            this.Activated += new System.EventHandler(this.button3_Click);
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox1;
+        private System.Windows.Forms.MaskedTextBox maskedTextBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+    }
+}
+
---
+++ interface_fredy_v2/Form1.cs
@@ -0,0 +1,245 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using System.Threading;
+
+namespace interface_fredy_v2
+{
+    public partial class Form1 : Form
+    {
+
+        private bool privodActivated = false;
+        int counter = 0;
+        //Thread readThread = new Thread(Read);
+        public Form1()
+        {
+            InitializeComponent();
+            this.PreviewKeyDown += new System.Windows.Forms.PreviewKeyDownEventHandler(this.Form1_PreviewKeyDown);
+        }
+
+        private void Form1_PreviewKeyDown(object sender, PreviewKeyDownEventArgs e)
+        {
+            MessageBox.Show("Отладка в форме: " + e.KeyCode.ToString());
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            UpdateComList();
+            button4.Enabled = false;
+            maskedTextBox1.Mask = "000";
+            maskedTextBox2.Mask = "000";
+            button5.Enabled = false;
+        }
+        private void UpdateComList()
+        {
+            comboBox1.Items.Clear();
+            //foreach (string com in SerialPort.GetPortNames()) comboBox1.Items.Add(com);
+            comboBox1.Items.AddRange(SerialPort.GetPortNames());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            UpdateComList();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (serialPort1.IsOpen)
+                {
+                    //readThread.Abort();
+                    serialPort1.Close();
+                    label1.Text = "Статус соединения: Отключено";
+                    button2.Text = "Соединиться";
+                    comboBox1.Enabled = true;
+                    button1.Enabled = true;
+
+                    button4.Enabled = false;
+                    button5.Enabled = false;
+                    //деактивация приводов
+                    button3.Enabled = false;
+                    deactivatePrivod();
+                }
+                else if (comboBox1.SelectedIndex >= 0)
+                {
+                    //readThread.Start();
+                    serialPort1.BaudRate = 57600;
+                    serialPort1.PortName = comboBox1.SelectedItem.ToString();
+                    serialPort1.Handshake = Handshake.None;
+                    serialPort1.ReadTimeout = 210; //210 здесь если в роботе 100
+                    serialPort1.WriteTimeout = -1; //может ждать бесконечно
+                    serialPort1.Open();
+                    label1.Text = "Статус соединения: Подключено";
+                    button2.Text = "Отсоединиться";
+                    comboBox1.Enabled = false;
+                    button1.Enabled = false;
+
+                    button4.Enabled = true;
+                    button5.Enabled = true;
+
+                    //активация кнопки приводов
+                    button3.Enabled = true;
+                }
+                else MessageBox.Show("Ошибка: Выберите COM порт!");
+            }
+            catch (Exception error)
+            {
+                MessageBox.Show("Ошибка 1: " + error.Message);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (privodActivated)
+            {
+                //button4.Enabled = true;
+                deactivatePrivod();
+            }
+            else if (serialPort1.IsOpen)
+            {
+                //button4.Enabled = false;
+                activatePrivod();
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //чтобы при закрытии формы соединение закрывалось и у других приложений был доступ к порту
+            if (serialPort1.IsOpen) serialPort1.Close();
+        }
+
+        private void button3_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (privodActivated)
+            {
+                switch (e.KeyCode.ToString())
+                {
+                    case "Q":
+                        serialPort1.Write("q");
+                        delay_counter();
+                        break;
+                    case "W":
+                        serialPort1.Write("w");
+                        delay_counter();
+                        break;
+                    case "E":
+                        serialPort1.Write("e");
+                        delay_counter();
+                        break;
+                    case "A":
+                        serialPort1.Write("a");
+                        delay_counter();
+                        break;
+                    case "S":
+                        serialPort1.Write("s");
+                        delay_counter();
+                        break;
+                    case "D":
+                        serialPort1.Write("d");
+                        delay_counter();
+                        break;
+                    case "Escape":
+                        deactivatePrivod();
+                        break;
+                }
+                //readThread.Join();
+            }
+        }
+
+        private void activatePrivod()
+        {
+            privodActivated = true;
+            label2.Text = "Статус приводов: Активированы";
+            button3.Text = "Деактивировать приводы";
+            //MessageBox.Show("Статус: Приводы активированы");
+        }
+
+        private void deactivatePrivod()
+        {
+            privodActivated = false;
+            label2.Text = "Статус приводов: Деактивированы";
+            button3.Text = "Активировать приводы";
+            //MessageBox.Show("Статус: Приводы деактивированы");
+        }
+
+        //public static void Read()
+        //{
+        //    while (true)
+        //         //(serialPort1.IsOpen)
+        //    {
+        //        try
+        //        {
+        //            //MessageBox.Show(serialPort1.ReadLine());
+        //            //textBox1.Text = serialPort1.ReadLine();
+        //        }
+        //        catch (TimeoutException error)
+        //        {
+        //            MessageBox.Show("Ошибка 2: " + error.Message);
+        //        }
+        //    }
+        //}
+
+        private void delay_counter()
+        {
+            try
+            {
+                //задержка решает
+                counter++;
+                label3.Text = "Выполнено операций: " + counter;
+                Thread.Sleep(40);
+            }
+            catch (TimeoutException error)
+            {
+                //MessageBox.Show("Ошибка 3: " + error.Message);
+            }
+        }
+        private void refreshConsole()
+        {
+            try
+            {
+                //serialPort1.ReadTimeout = -1;
+                while (serialPort1.BytesToRead > 0) textBox1.Text += serialPort1.ReadLine() + "\n";
+                textBox1.SelectionStart = textBox1.Text.Length;
+                textBox1.ScrollToCaret();
+            }
+            catch (TimeoutException error)
+            {
+                MessageBox.Show("Ошибка 3: " + error.Message);
+            }
+            finally
+            { //serialPort1.ReadTimeout = 210; }
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            button3.Enabled = false;
+            deactivatePrivod();
+            if (serialPort1.IsOpen) serialPort1.Write("x" + maskedTextBox1.Text);
+            //MessageBox.Show("x" + maskedTextBox1.Text);
+            Thread.Sleep(2000);
+            if (serialPort1.IsOpen) serialPort1.Write("c" + maskedTextBox2.Text);
+            //MessageBox.Show("c" + maskedTextBox2.Text);
+            Thread.Sleep(2000);
+            refreshConsole();
+            button3.Enabled = true;
+            button3.Focus();
+            activatePrivod();
+            counter += 2;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            refreshConsole();
+        }
+    }
+}
